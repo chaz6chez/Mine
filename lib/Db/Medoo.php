d6112f2aa9198a9b2491efcd1fb57d14227973a8
chaz6chez <250220719@qq.com>
2020-12-02 17:45:03 +0800

    修复medoo option无法生效问题

--- lib/Db/Medoo.php
+++ lib/Db/Medoo.php
@@ -322,21 +322,17 @@ class Medoo {
     }
 
     public function connection() {
-        $option = isset($this->options['option']) ? $this->options['option'] : [];
         # 设置抛出异常
-        $option = array_merge($option,[
+        $this->option = $this->option + [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]);
+        ];
 
         $this->pdo = new PDO(
             $this->dsn,
             isset($this->options['username']) ? $this->options['username'] : null,
             isset($this->options['password']) ? $this->options['password'] : null,
-            $option
+            $this->option
         );
-
-        # 设置抛出异常
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     public function closeConnection() {
